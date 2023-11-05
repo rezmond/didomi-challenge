63929550a89e9ec097ad2a811699b1fb918fd761
rezmond <rezx10m@gmail.com>
2023-11-05 10:13:02 +0400

    Introduce the tests environment

---
+++ src/shared/lib/tests.tsx
@@ -0,0 +1,22 @@
+import { ConsentApi } from '@/types';
+import { ConsentApiContext } from './consentApiContext';
+import {
+  type RenderOptions as RenderOptionsBase,
+  type RenderResult,
+  render as renderBase
+} from '@testing-library/react';
+import { FC, ReactElement, ReactNode } from 'react';
+
+type RenderOptions = Omit<RenderOptionsBase, 'queries'> & {
+  consentApi: ConsentApi
+}
+
+export const render = (ui: ReactElement, { consentApi, ...options}: RenderOptions ): RenderResult => {
+  const AllTheProviders: FC<{children: ReactNode}> = ({ children }) => (
+    <ConsentApiContext.Provider value={consentApi} >
+        { children }
+      </ConsentApiContext.Provider>
+    )
+
+  return renderBase(ui, { wrapper: AllTheProviders, ...options });
+}
